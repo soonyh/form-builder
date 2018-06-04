afa224831e10096614720c5010f37e1d9703b1c9
soonyh <soonyh@126.com>
2018-06-04 17:24:55 +0800

    添加确认框

---
+++ mock/rules.js
@@ -0,0 +1,32 @@
+  $.ajaxSetup({
+      data: Math.random().toString()
+  });
+  $(document).ajaxSuccess(function(event, jqXHR, ajaxOptions) {
+      var data = ajaxOptions.data == undefined ? null : ajaxOptions.data
+          // 只能打印出get类型的传参
+      console.group('#####################')
+      console.info('请求地址：%s', ajaxOptions.url);
+      console.log('  提交数据: %o', decodeURIComponent(data));
+      if (jqXHR.responseJSON) {
+          console.log('  返回数据: %o', jqXHR.responseJSON)
+      };
+      console.groupEnd();
+  });
+  var Random = Mock.Random;
+  Mock.setup({
+      timeout: '300'
+  });
+  
+  Mock.mock(/list/, {
+      "result": "1",
+      "msg": "登录成功",
+      "list|266": [{
+          "id|+1": 1,
+          "title": "@cword(5,15)",
+          "type": 1,
+          "tips":"",
+          "newLine":"0",
+          "display":"1",
+          "readOnly":"0"
+      }]
+  });
